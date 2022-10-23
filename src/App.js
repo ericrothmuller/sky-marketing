586f8a07f941109cbb01853c9e206e61086b73c7
ericrothmuller <eric@skymarketingco.com>
2022-10-22 22:32:29 -0700

    Updated nav items

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
 	from 'react-router-dom';
-import Home from './pages';
+import Home from './pages/home';
 import About from './pages/about';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
