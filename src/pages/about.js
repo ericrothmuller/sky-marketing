e72a03f73fd1dc0c5a715f6a26d5f3697470bde0
ericrothmuller <eric@skymarketingco.com>
2022-10-30 20:23:05 -0700

    Added intro text to home and about us pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ const About = () => {
 return (
 	<main className="pageHeadingDiv">
 	<h1 className="pageTitle">About Us</h1>
-	<p className="pageParagraph">Test paragraph</p>
+	<p className="pageParagraph">We are a web design and online marketing company out of Northern California.</p>
 	</main>
 );
 };
