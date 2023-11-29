3652db7c2c4c4e0eb741a371bc13e2a063383468
ericrothmuller <eric@skymarketingco.com>
2023-11-29 04:22:26 -0800

    Added alt text to images on service page

--- src/pages/services.js
+++ src/pages/services.js
@@ -8,13 +8,13 @@ return (
 	<h1 className="pageTitle">Our Services</h1>
 	<h2>Website Design</h2>
 	<p>We create websites on either WordPress or React. We can create anything from small business websites to large eCommerce websites with either platform. Which one we create your website on is completely up to you.</p>
-	<p><img src={WordPressImage} /></p>
+	<p><img src={WordPressImage} alt="WordPress Logo" /></p>
 	<p>WordPress is a platform that over 40% of all websites are built on. It is the most popular content management system in the world.</p>
 	<p>WordPress allows for a website to be made relatively quick, has many templates to choose from, and can be fully customized. It's a good option for small to medium-sized businesses or personal websites. </p>
 	<p>Typical Design Cost Range: $1,500 - $5,000.
 	<br/><em>*Prices vary depending on scope and scale of the project.</em></p>
 
-	<p><img src={ReactLogo} /></p>
+	<p><img src={ReactLogo} alt="React Logo" /></p>
 	<p>React is used by over 1.8 million websites and is one of the most beloved web frameworks by developers.</p>
 	<p>A React website does take a significant amount of more time to create and maintain than a WordPress website but the overall performance of a React website is superior. React websites typically load faster which is important for things like SEO and a users experience. React websites are more custom and extremely scalable, making it a good choice for anything from small to large websites. This website that you are on now is built with React.</p>
 	<p>Typical Design Cost Range: $10,000 - $100,000.
