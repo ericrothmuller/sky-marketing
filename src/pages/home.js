233a5e9922d2a1806e7e375160967b96c76c7a86
ericrothmuller <eric@skymarketingco.com>
2023-11-30 23:33:10 -0800

    Added contact information on the homepage.

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ return (
 	<main>
 	<h1>Welcome to Sky Marketing</h1>
 	<p>We are a web design and online marketing company out of Northern California.</p>
+	<p><a href="/contact/">Contact us</a> today at <a href="tel:707-843-2047">707-843-2047</a></p>
 	</main>
 );
 };
