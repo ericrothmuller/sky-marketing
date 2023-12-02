d46dc7109d6f7061a6644b9ffa6a2862b413ee66
ericrothmuller <eric@skymarketingco.com>
2023-12-01 22:37:28 -0800

    Added info about Eric Rothmuller (myself) on the about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,12 @@ import React from "react";
 const About = () => {
 return (
 	<main className="pageHeadingDiv">
-	<h1 className="pageTitle">About Us</h1>
-	<p className="pageParagraph">We are a web design and online marketing company out of Northern California.</p>
-	<h2 className="pageTitle">Meet Our Founder</h2>
-	<p className="pageParagraph">Eric Rothmuller has been doing web design since 2010 and went to U.C. Berkeley for Coding. He is a full-stack coder (front-end and back-end) and an expert in HTML, CSS, JavaScript, React, WordPress, UI/UX Design, Responsive Web Design, Web Hosting, Search Engine Optimization, Social Media Marketing, Node.js, Express.js, SQL, MongoDB, and more.</p>
+		<h1 className="pageTitle">About Us</h1>
+		<p className="pageParagraph">We are a web design and online marketing company out of Northern California.</p>
+		<h2 className="pageTitle">Meet Our Founder</h2>
+		<p className="pageParagraph">Hello. My name is Eric Rothmuller and I have been doing web design and online marketing since 2010. I went to the Santa Rosa JC for Marketing and U.C. Berkeley for Full-Stack Programming.</p>
+		<p>My web design and coding expertise is in HTML, CSS, JavaScript, React, WordPress, UI/UX Design, Responsive Web Design, Web Hosting, Node.js, Express.js, SQL, MongoDB, and more.</p>
+		<p>My marketing expertise is primarily in online marketing techniques such as Search Engine Optimization, Social Media Marketing, Digital Ads, eCommerce Management, Graphic Design, E-Mail Marketing, and more.</p>
 	</main>
 );
 };
