0f7105ac9ded94a8d42c7a22d0d9c17d811733e0
ericrothmuller <eric@skymarketingco.com>
2023-12-04 15:14:19 -0800

    Added text to the homepage, added a centeredDiv class to the main element of the homepage, added a services button to the homepage, and added CSS styling the button.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,11 @@ import '.././App.css';
 
 const Home = () => {
 return (
-	<main>
+	<main className="centeredDiv">
 	<h1>Welcome to Sky Marketing</h1>
 	<p>We are a web design and online marketing company out of Northern California.</p>
+	<p>From the initial design of a brand new website to managing the end-product and everything in between.</p>
+	<a href="/services"><button>Our Services</button></a>
 	<p><a href="/contact/">Contact us</a> today at <a href="tel:707-843-2047">707-843-2047</a></p>
 	</main>
 );
