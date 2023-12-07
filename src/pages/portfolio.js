0aa7feb10aaf5535c95e0ebd77cf7a011dd15200
ericrothmuller <eric@skymarketingco.com>
2023-12-06 18:02:00 -0800

    Started adding portfolio items

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,7 +4,19 @@ const Portfolio = () => {
 return (
 	<main>
 	<h1>Portfolio</h1>
-	<p>Test paragraph</p>
+
+	<p className="centeredText"><a href="#WorkedFor">Whom We've Done Work For</a> | <a href="#BusinessWebsites">Business Websites</a></p>
+
+	<hr className="beyondMain"/>
+
+	<h2 id="WorkedFor">Whom We've Done Work For</h2>
+	<p>Anytime Fitness</p>
+
+	<hr className="beyondMain"/>
+
+	<h2 id="BusinessWebsites">Businesses Websites</h2>
+	<p>test text</p>
+
 	</main>
 );
 };
