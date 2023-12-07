c2deb042e1dbb97b9cee8cdb94c10a0bb2182f0b
ericrothmuller <eric@skymarketingco.com>
2023-12-06 19:43:47 -0800

    Added portfolio items

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,7 +10,59 @@ return (
 	<hr className="beyondMain"/>
 
 	<h2 id="WorkedFor">Whom We've Done Work For</h2>
-	<p>Anytime Fitness</p>
+	<ul>
+		<li>Anytime Fitness</li>
+		<li>Flying Goat Coffee</li>
+		<li>Elite MD</li>
+		<li>Flakey Cream Coffee and Donuts</li>
+		<li>Red Bird Bakery</li>
+		<li>Applied Signs</li>
+		<li>Trashy Behavior</li>
+		<li>Child Parent Institute</li>
+		<li>Gladstone Clinic</li>
+		<li>Home Pro 360</li>
+		<li>Chao-Santana Farm</li>
+		<li>Belay Advertising</li>
+		<li>Swicegood Civil Engineering</li>
+		<li>K9 Activity Club</li>
+		<li>BLS Auto Detail</li>
+		<li>Tag-Dis Tag-Dat</li>
+		<li>Sonoma Crust</li>
+		<li>Rancho Maria Winery</li>
+		<li>Four Seasons Vineyard Management</li>
+		<li>Murder Ridge Winery</li>
+		<li>DeWitte Construction</li>
+		<li>Thrive & Lead</li>
+		<li>United States Tennis Association</li>
+		<li>Her N Her</li>
+		<li>Firefly Athletics</li>
+		<li>BLS Auto Detail</li>
+		<li>A & D Mobile Automotive Repair</li>
+		<li>Golden Bear Financial</li>
+		<li>Ad Taxi</li>
+		<li>Heartstone Media</li>
+		<li>Kastania Vineyards</li>
+		<li>Cait Hudson Realty</li>
+		<li>Redwood Coast Staffords</li>
+		<li>Redwood Empire Fire</li>
+		<li>Global Teff</li>
+		<li>Avenue Car Wash</li>
+		<li>Blue Heron IPM Pest Control</li>
+		<li>North Bay Auto</li>
+		<li>Image Athletic</li>
+		<li>Sonoma Barrel</li>
+		<li>Bliss Inks</li>
+		<li>Home Sellers Realty</li>
+		<li>iHeart Media</li>
+		<li>First Class Limo Service</li>
+		<li>Laser Tag of Santa Rosa</li>
+		<li>Sac Cool Sculpt</li>
+		<li>Pristine Window Solutions</li>
+		<li>Green Storx LLC</li>
+		<li>Funny Picturesque</li>
+		<li>Executive Charters</li>
+		
+	</ul>
 
 	<hr className="beyondMain"/>
 
