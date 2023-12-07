299adbde7dab58dac1e33b6e24be85b9e65a593d
ericrothmuller <eric@skymarketingco.com>
2023-12-06 22:28:48 -0800

    Added 2 column layout for the whom we've done work for section, rearranged lists, added subtext, edited general page text, and make the columns responsive.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,65 +9,70 @@ return (
 
 	<hr className="beyondMain"/>
 
-	<h2 id="WorkedFor">Whom We've Done Work For</h2>
-	<ul>
-		<li>Anytime Fitness</li>
-		<li>Flying Goat Coffee</li>
-		<li>Elite MD</li>
-		<li>Flakey Cream Coffee and Donuts</li>
-		<li>Red Bird Bakery</li>
-		<li>Applied Signs</li>
-		<li>Trashy Behavior</li>
-		<li>Child Parent Institute</li>
-		<li>Gladstone Clinic</li>
-		<li>Home Pro 360</li>
-		<li>Chao-Santana Farm</li>
-		<li>Belay Advertising</li>
-		<li>Swicegood Civil Engineering</li>
-		<li>K9 Activity Club</li>
-		<li>BLS Auto Detail</li>
-		<li>Tag-Dis Tag-Dat</li>
-		<li>Sonoma Crust</li>
-		<li>Rancho Maria Winery</li>
-		<li>Four Seasons Vineyard Management</li>
-		<li>Murder Ridge Winery</li>
-		<li>DeWitte Construction</li>
-		<li>Thrive & Lead</li>
-		<li>United States Tennis Association</li>
-		<li>Her N Her</li>
-		<li>Firefly Athletics</li>
-		<li>BLS Auto Detail</li>
-		<li>A & D Mobile Automotive Repair</li>
-		<li>Golden Bear Financial</li>
-		<li>Ad Taxi</li>
-		<li>Heartstone Media</li>
-		<li>Kastania Vineyards</li>
-		<li>Cait Hudson Realty</li>
-		<li>Redwood Coast Staffords</li>
-		<li>Redwood Empire Fire</li>
-		<li>Global Teff</li>
-		<li>Avenue Car Wash</li>
-		<li>Blue Heron IPM Pest Control</li>
-		<li>North Bay Auto</li>
-		<li>Image Athletic</li>
-		<li>Sonoma Barrel</li>
-		<li>Bliss Inks</li>
-		<li>Home Sellers Realty</li>
-		<li>iHeart Media</li>
-		<li>First Class Limo Service</li>
-		<li>Laser Tag of Santa Rosa</li>
-		<li>Sac Cool Sculpt</li>
-		<li>Pristine Window Solutions</li>
-		<li>Green Storx LLC</li>
-		<li>Funny Picturesque</li>
-		<li>Executive Charters</li>
-		<li>Healdsburg Animal Shelter</li>
-		
-	</ul>
+	<h2 id="WorkedFor">Some of the companies we've done work for:</h2>
+	<div className="row">
+		<div className="columnLeft">
+			<ul>
+				<li>Anytime Fitness</li>
+				<li>Firefly Athletics</li>
+				<li>Elite MD</li>
+				<li>Image Athletic</li>
+				<li>iHeart Media</li>
+				<li>Ad Taxi</li>
+				<li>Bruk Hair Studio</li>
+				<li>Thrive & Lead</li>
+				<li>Cait Hudson Realty</li>
+				<li>Home Pro 360</li>
+				<li>Chao-Santana Farm</li>
+				<li>Belay Advertising</li>
+				<li>Gladstone Clinic</li>
+				<li>Swicegood Civil Engineering</li>
+				<li>Pristine Window Solutions</li>
+				<li>Green Storx LLC</li>
+				<li>Funny Picturesque</li>
+				<li>Tag-Dis Tag-Dat</li>
+				<li>Bliss Inks</li>
+				<li>Home Sellers Realty</li>
+				<li>Her N Her</li>
+				<li>Trashy Behavior</li>
+				<li>First Class Limo Service</li>
+				<li>Executive Charters</li>
+			</ul>
+		</div>
+		<div className="columnRight">
+			<ul>
+				<li>Flying Goat Coffee</li>
+				<li>Child Parent Institute</li>
+				<li>Sonoma Crust</li>
+				<li>Red Bird Bakery</li>
+				<li>Kastania Vineyards</li>
+				<li>Murder Ridge Winery</li>
+				<li>Rancho Maria Winery</li>
+				<li>Four Seasons Vineyard Management</li>
+				<li>Sonoma Barrel</li>
+				<li>BLS Auto Detail</li>
+				<li>Flakey Cream Coffee and Donuts</li>
+				<li>Golden Bear Financial</li>
+				<li>Laser Tag of Santa Rosa</li>
+				<li>Avenue Car Wash</li>
+				<li>Redwood Coast Staffords</li>
+				<li>Healdsburg Animal Shelter</li>
+				<li>A & D Mobile Automotive Repair</li>
+				<li>Redwood Empire Fire</li>
+				<li>Global Teff</li>
+				<li>Blue Heron IPM Pest Control</li>
+				<li>North Bay Auto</li>
+				<li>K9 Activity Club</li>
+				<li>Applied Signs</li>
+				<li>DeWitte Construction</li>	
+			</ul>
+		</div>
+		<p> <em className="subFont">*This is not all of the companies we've done work for but this sample should give you an idea of the type of companies we've done work for. Work done includes web design, SEO, web management, and more!</em></p>
+	</div>
 
 	<hr className="beyondMain"/>
 
-	<h2 id="BusinessWebsites">Businesses Websites</h2>
+	<h2 id="BusinessWebsites">Business Websites</h2>
 	<p>test text</p>
 
 	</main>
