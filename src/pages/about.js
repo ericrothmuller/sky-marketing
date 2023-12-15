7d1f1cc255fa74f8778759b4c082fc23432dca3c
ericrothmuller <eric@skymarketingco.com>
2023-12-14 21:59:57 -0800

    Added founders box on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,11 +6,13 @@ return (
 	<main className="pageHeadingDiv">
 		<h1 className="pageTitle">About Us</h1>
 		<p className="pageParagraph">We are a web design and online marketing company out of Northern California.</p>
-		<h2 className="pageTitle">Meet Our Founder</h2>
-		<img src={eric} alt="Eric Rothmuller" className="roundedCorners" />
-		<p className="pageParagraph">Hello. My name is Eric Rothmuller and I have been doing web design and online marketing since 2010. I went to the Santa Rosa JC for Marketing and U.C. Berkeley for Full-Stack Programming.</p>
-		<p>My web design and coding expertise is in HTML, CSS, JavaScript, React, WordPress, UI/UX Design, Responsive Web Design, Web Hosting, Node.js, Express.js, SQL, MongoDB, and more.</p>
-		<p>My marketing expertise is primarily in online marketing techniques such as Search Engine Optimization, Social Media Marketing, Digital Ads, eCommerce Management, Graphic Design, E-Mail Marketing, and more.</p>
+		<div className="founderBox">
+			<h2 className="pageTitle alignSelfCenter">Meet Our Founder</h2>
+			<img src={eric} alt="Eric Rothmuller" className="aboutImage" />
+			<p className="pageParagraph">Hello. My name is Eric Rothmuller and I have been doing web design and online marketing since 2010. I went to the Santa Rosa JC for Marketing and U.C. Berkeley for Full-Stack Programming.</p>
+			<p>My web design and coding expertise is in HTML, CSS, JavaScript, React, WordPress, UI/UX Design, Responsive Web Design, Web Hosting, Node.js, Express.js, SQL, MongoDB, and more.</p>
+			<p>My marketing expertise is primarily in online marketing techniques such as Search Engine Optimization, Social Media Marketing, Digital Ads, eCommerce Management, Graphic Design, E-Mail Marketing, and more.</p>
+		</div>
 	</main>
 );
 };
