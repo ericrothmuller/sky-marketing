7f04930a4c0c1aed9c8bf5a3a9611f41d4b39708
ericrothmuller <eric@skymarketingco.com>
2023-12-24 21:30:15 -0800

    Edited contact page header text.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
 return (
 	<main>
-	<h1>Get In Contact</h1>
+	<h1>Get In Touch</h1>
 	<p>Call us at <a href="tel:707-843-2047">707-843-2047</a> or email us at <a href="mailto:info@skymarketingco.com">info@skymarketingco.com</a></p>
 	<p>Need to create a form here.</p>
 	</main>
