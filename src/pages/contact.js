621b935e65f1d1f259a9227a37fdfc646a2147d0
ericrothmuller <eric@skymarketingco.com>
2024-01-01 09:28:26 -0800

    Added hours of operation and rearranged contact us page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,17 @@ const Contact = () => {
 return (
 	<main>
 	<h1>Get In Touch</h1>
-	<p>Call us at <a href="tel:707-843-2047">707-843-2047</a> or email us at <a href="mailto:info@skymarketingco.com">info@skymarketingco.com</a></p>
-	<p>Need to create a form here.</p>
+	<p><strong>Phone:</strong> <a href="tel:707-843-2047">707-843-2047</a></p>
+	<p><strong>Email:</strong> <a href="mailto:info@skymarketingco.com">info@skymarketingco.com</a></p>
+	&nbsp;
+	<p><strong>Hours of Operation:</strong></p>
+	<p><strong>Monday:</strong> 8:00AM-6:00PM</p>
+	<p><strong>Tuesday:</strong> 8:00AM-6:00PM</p>
+	<p><strong>Wednesday:</strong> 8:00AM-6:00PM</p>
+	<p><strong>Thursday:</strong> 8:00AM-6:00PM</p>
+	<p><strong>Friday:</strong> 8:00AM-6:00PM</p>
+	<p><strong>Saturday:</strong> Closed</p>
+	<p><strong>Sunday:</strong> Closed</p>
 	</main>
 );
 };
