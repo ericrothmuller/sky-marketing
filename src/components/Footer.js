4f37265bac6bf2401d0d8f460ce342386ed8390b
ericrothmuller <eric@skymarketingco.com>
2024-01-08 14:09:43 -0800

    Switched the footer to just one row of three columns and switched the content around and bolded the phone number and hours of operation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,15 @@ function Footer() {
     <footer>
       <div className="footer-columns">
         <div className="footer-column">
-            <p>Phone: 707-843-2047</p>
-            <p>Kelseyville, CA & Louisville, KY</p>
+          <p className="bolded">Phone: 707-843-2047</p>
+          <p><a href="/privacy-policy/">Privacy Policy</a> | <a href="/terms-and-conditions/">Terms & Conditions</a> | <a href="/accessability">Accessibility</a></p>
         </div>
         <div className="footer-column">
         <a href="/home"><img src={Logo} className="footerLogo" alt="Sky Marketing Logo" /></a>
         </div>
         <div className="footer-column">
-            <p>Hours of Operation:</p>
-            <p>Monday - Friday: 8:00 AM - 5:00 PM</p>
-        </div>
-      </div>
-      <div className="footer-columns">
-        <div className="footer-column">
-            <p>Phone: 707-843-2047</p>
-            <p>Kelseyville, CA & Louisville, KY</p>
-        </div>
-        <div className="footer-column">
-            <p><a href="/privacy-policy/">Privacy Policy</a> | <a href="/terms-and-conditions/">Terms & Conditions</a> | <a href="/accessability">Accessibility</a></p>
-            <p>© 2023 Sky Marketing. All rights reserved.</p>
-        </div>
-        <div className="footer-column">
-            <p>Hours of Operation:</p>
-            <p>Monday - Friday: 8:00 AM - 5:00 PM</p>
+          <p className="bolded">Open: Monday - Friday: 8:00 AM - 5:00 PM PST</p>
+          <p>© 2023 Sky Marketing. All rights reserved.</p>
         </div>
       </div>
     </footer>
