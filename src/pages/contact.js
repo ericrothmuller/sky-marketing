0a32eb1adc09edca6841cdbc469410f5cb6131ac
Eric Rothmuller <eric@skymarketingco.com>
2025-04-01 12:10:40 -0700

    Added contact us image with formatting

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactUsImg from "..//images/Contact-Us-Bottom.png";
 
 const Contact = () => {
 return (
@@ -8,18 +9,23 @@ return (
 		<p className="centered">We look forward to speaking with you!</p>
 		<hr className="beyondMain"/>
 		<div className="columnLeft">
-			<p><strong>Phone:</strong> <a href="tel:707-843-2047">707-843-2047</a></p>
-			<p><strong>Email:</strong> <a href="mailto:info@skymarketingco.com">info@skymarketingco.com</a></p>
+		<div class="centered">
+				<p><strong>Hours of Operation:</strong></p>
+				<p><strong>Monday:</strong> 8:00AM-6:00PM</p>
+				<p><strong>Tuesday:</strong> 8:00AM-6:00PM</p>
+				<p><strong>Wednesday:</strong> 8:00AM-6:00PM</p>
+				<p><strong>Thursday:</strong> 8:00AM-6:00PM</p>
+				<p><strong>Friday:</strong> 8:00AM-6:00PM</p>
+				<p><strong>Saturday:</strong> Closed</p>
+				<p><strong>Sunday:</strong> Closed</p>
+			</div>
 		</div>
 		<div className="columnRight">
-			<p><strong>Hours of Operation:</strong></p>
-			<p><strong>Monday:</strong> 8:00AM-6:00PM</p>
-			<p><strong>Tuesday:</strong> 8:00AM-6:00PM</p>
-			<p><strong>Wednesday:</strong> 8:00AM-6:00PM</p>
-			<p><strong>Thursday:</strong> 8:00AM-6:00PM</p>
-			<p><strong>Friday:</strong> 8:00AM-6:00PM</p>
-			<p><strong>Saturday:</strong> Closed</p>
-			<p><strong>Sunday:</strong> Closed</p>
+		<div class="centered">
+			<	p><strong>Phone:</strong> <a href="tel:707-843-2047">707-843-2047</a></p>
+				<p><strong>Email:</strong> <a href="mailto:info@skymarketingco.com">info@skymarketingco.com</a></p>
+				<img src={ContactUsImg} alt="Contact Us Image" className="contactImage"/>
+			</div>
 		</div>
 	</div>
 	</main>
