2ea472736307d92cecde29c0db360c1da1bd5f8a
Eric Rothmuller <eric@skymarketingco.com>
2025-04-02 12:51:58 -0700

    Edited home page to make the your success area title centered and added a light blue background behind the entire div. I also edited the contact us text and moved the services button up a little bit on the page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,6 +13,8 @@ return (
 	<h2>Elevate Your Online Presence and Fuel Your Business Growth</h2>
 	<p>At Sky Marketing, we are your trusted partner in the digital world, dedicated to helping businesses thrive in the online realm. Our comprehensive suite of services, including web design, web management, search engine optimization, and more, is meticulously crafted to empower your brand and drive sustainable success. With a team of experts passionate about delivering results, we're here to transform your online presence into a powerful asset that attracts, engages, and converts. Explore our services, and let's embark on a journey to unlock your business's full potential in the digital age.</p>
 
+	<hr className="beyondMain"/>
+	
 	<h2>Our Services</h2>
 	<h3>Web Design That Captivates</h3>
 	<p>Your website is your digital storefront, and first impressions matter. Our creative web design team crafts visually stunning, user-friendly websites that leave a lasting impact. Whether you're starting from scratch or seeking a redesign, we'll ensure your website not only looks exceptional but also delivers a seamless user experience.</p>
@@ -26,23 +28,21 @@ return (
 	<h3>More Than Just Services</h3>
 	<p>We go beyond traditional marketing services. We provide data-driven insights, a personalized approach, and continuous support to help you navigate the ever-changing digital landscape. Our goal is not just to meet your expectations but to exceed them.</p>
 
-	<h3>Your Success, Our Mission</h3>
-	<p>At Sky Marketing, your success is our mission. We take pride in our commitment to delivering tangible results that impact your bottom line. With a dedicated team of digital experts, cutting-edge technology, and a client-centric approach, we're here to drive your business forward in the digital age. Whether you're a small startup or an established enterprise, let's partner together to create a digital strategy tailored to your unique needs and goals.</p>
-
-	<h3>Get Started Today</h3>
-	<p>Ready to take your online presence to the next level? Contact us to schedule a consultation and discover how Sky Marketing can help your business thrive in the digital era. We're excited to be your partner on this journey to success.</p>
-
-	<p>Elevate. Engage. Excel. With Sky Marketing.</p>
-
 	<div className="centered">
 		<Link to="/services"><button className="normalButton">Our Services</button></Link>
 	</div>
 
-	<hr className="beyondMain"/>
+<br />
 
-	<h2>Get In Touch</h2>
-	<p>Contact us today to get started by calling us at <a href="tel:1-707-843-2047">707-843-2047</a> or by emailing us at <a href="mailto:eric@skymarketingco.com">eric@skymarketingco.com</a></p>
+	<div className="coloredDiv">
+		<h2 className="centered">Your Success, Our Mission</h2>
+		<p>At Sky Marketing, your success is our mission. We take pride in our commitment to delivering tangible results that impact your bottom line. With a dedicated team of digital experts, cutting-edge technology, and a client-centric approach, we're here to drive your business forward in the digital age. Whether you're a small startup or an established enterprise, let's partner together to create a digital strategy tailored to your unique needs and goals.</p>
+	</div>
 
+	<h2>Get Started Today</h2>
+	<p>Ready to take your online presence to the next level? Contact us to schedule a consultation and discover how Sky Marketing can help your business thrive in the digital era. We're excited to be your partner on this journey to success.</p>
+	<p>Elevate. Engage. Excel. With Sky Marketing.</p>
+	<p>Contact us today to get started by calling <a href="tel:1-707-843-2047">707-843-2047</a> or by emailing Eric at <a href="mailto:eric@skymarketingco.com">eric@skymarketingco.com</a></p>
 	</main>
 );
 };
