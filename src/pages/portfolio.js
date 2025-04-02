6771d9309929320f6130a14a9867d57731e430cd
Eric Rothmuller <eric@skymarketingco.com>
2025-04-01 20:27:42 -0700

    Added a couple companies we've done work for and added the class businessesWorkedForDiv which gives margin to the columns of business names

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -25,70 +25,75 @@ return (
 	<hr className="beyondMain"/>
 
 	<h2 id="WorkedFor"><span className="underlineText">Some</span> of the companies we've done work for:</h2>
-	<div className="row">
-		<div className="columnLeft">
-			<ul>
-				<li>Anytime Fitness</li>
-				<li>Firefly Athletics</li>
-				<li>Elite MD</li>
-				<li>Image Athletic</li>
-				<li>JH Baseball Academy</li>
-				<li>Future Life Today</li>
-				<li>Bruk Hair Studio</li>
-				<li>Thrive & Lead</li>
-				<li>Cait Hudson Realty</li>
-				<li>Home Pro 360</li>
-				<li>Chao-Santana Farm</li>
-				<li>Belay Advertising</li>
-				<li>Gladstone Clinic</li>
-				<li>Swicegood Civil Engineering</li>
-				<li>Pristine Window Solutions</li>
-				<li>Green Storx LLC</li>
-				<li>Funny Picturesque</li>
-				<li>Tag-Dis Tag-Dat</li>
-				<li>Bliss Inks</li>
-				<li>Home Sellers Realty</li>
-				<li>Her N Her</li>
-				<li>Trashy Behavior</li>
-				<li>First Class Limo Service</li>
-				<li>Executive Charters</li>
-				<li>o2 Resort and Cabanas</li>
-				<li>Elk's Lodge - Oceano</li>
-			</ul>
+	<div className="businessesWorkedForDiv">
+		<div className="row">
+			<div className="columnLeft">
+				<ul>
+					<li>Anytime Fitness</li>
+					<li>Firefly Athletics</li>
+					<li>Elite MD</li>
+					<li>Image Athletic</li>
+					<li>JH Baseball Academy</li>
+					<li>Future Life Today</li>
+					<li>Bruk Hair Studio</li>
+					<li>Thrive & Lead</li>
+					<li>Cait Hudson Realty</li>
+					<li>Home Pro 360</li>
+					<li>Chao-Santana Farm</li>
+					<li>Belay Advertising</li>
+					<li>Gladstone Clinic</li>
+					<li>Swicegood Civil Engineering</li>
+					<li>Pristine Window Solutions</li>
+					<li>Green Storx LLC</li>
+					<li>Funny Picturesque</li>
+					<li>Tag-Dis Tag-Dat</li>
+					<li>Bliss Inks</li>
+					<li>Home Sellers Realty</li>
+					<li>Her N Her</li>
+					<li>Trashy Behavior</li>
+					<li>First Class Limo Service</li>
+					<li>Executive Charters</li>
+					<li>o2 Resort and Cabanas</li>
+					<li>Elk's Lodge - Oceano</li>
+					<li>USTA Kentucky</li>
+				</ul>
+			</div>
+			<div className="columnRight">
+				<ul>
+					<li>Flying Goat Coffee</li>
+					<li>Child Parent Institute</li>
+					<li>Sonoma Crust</li>
+					<li>Red Bird Bakery</li>
+					<li>Kastania Vineyards</li>
+					<li>Murder Ridge Winery</li>
+					<li>Rancho Maria Winery</li>
+					<li>Four Seasons Vineyard Management</li>
+					<li>Sonoma Barrel</li>
+					<li>BLS Auto Detail</li>
+					<li>Flakey Cream Coffee and Donuts</li>
+					<li>Golden Bear Financial</li>
+					<li>Laser Tag of Santa Rosa</li>
+					<li>Avenue Car Wash</li>
+					<li>Redwood Coast Staffords</li>
+					<li>Healdsburg Animal Shelter</li>
+					<li>A & D Mobile Automotive Repair</li>
+					<li>Redwood Empire Fire</li>
+					<li>Southern Arizona Guide</li>
+					<li>Global Teff</li>
+					<li>Blue Heron IPM Pest Control</li>
+					<li>North Bay Auto</li>
+					<li>K9 Activity Club</li>
+					<li>Applied Signs</li>
+					<li>DeWitte Construction</li>
+					<li>Zed's Outdoor Services</li>
+					<li>The Folde Laundry Service</li>
+				</ul>
+			</div>
 		</div>
-		<div className="columnRight">
-			<ul>
-				<li>Flying Goat Coffee</li>
-				<li>Child Parent Institute</li>
-				<li>Sonoma Crust</li>
-				<li>Red Bird Bakery</li>
-				<li>Kastania Vineyards</li>
-				<li>Murder Ridge Winery</li>
-				<li>Rancho Maria Winery</li>
-				<li>Four Seasons Vineyard Management</li>
-				<li>Sonoma Barrel</li>
-				<li>BLS Auto Detail</li>
-				<li>Flakey Cream Coffee and Donuts</li>
-				<li>Golden Bear Financial</li>
-				<li>Laser Tag of Santa Rosa</li>
-				<li>Avenue Car Wash</li>
-				<li>Redwood Coast Staffords</li>
-				<li>Healdsburg Animal Shelter</li>
-				<li>A & D Mobile Automotive Repair</li>
-				<li>Redwood Empire Fire</li>
-				<li>Global Teff</li>
-				<li>Blue Heron IPM Pest Control</li>
-				<li>North Bay Auto</li>
-				<li>K9 Activity Club</li>
-				<li>Applied Signs</li>
-				<li>DeWitte Construction</li>
-				<li>Zed's Outdoor Services</li>
-				<li>The Folde Laundry Service</li>
-			</ul>
-		</div>
+	</div>
 		<p> <em className="subFont">*This is not all of the companies we've done work for but this list of clients should give you an idea of the type of companies we've done work.
 		<br />*Work done includes web design, SEO, web management, and more.</em></p>
-	</div>
+
 
 	<hr className="beyondMain"/>
 
