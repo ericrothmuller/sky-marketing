9a2e07e658d89ca15adb18957f902405bfeed305
Eric Rothmuller <eric@skymarketingco.com>
2025-04-03 11:29:29 -0700

    Added Navigation folder and Nav.js and Hamburger.js components which will be for desktop and mobile navigation menus

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,13 @@
+export default function Nav(){
+    return (
+        <div>
+            <div className="navigation">
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </div>
+        </div>
+    )
+}
