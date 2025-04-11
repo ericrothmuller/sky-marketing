449bb7c02728ed449dcf36e6bc09345086b96d49
Eric Rothmuller <eric@skymarketingco.com>
2025-04-11 04:08:21 -0700

    Added an onClick function using useState to trigger the hamburger menu open or closed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
       <Link to="/" className="title">Sky Marketing</Link>
-      <div className="menu">
+      <div className="menu" onClick={() => {
+        setMenuOpen(!menuOpen)
+      }}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul>
+      <ul className={menuOpen ? "open" : ""}>
+        <li><NavLink to="/Home">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/services">Services</NavLink></li>
         <li><NavLink to="/portfolio">Portfolio</NavLink></li>
